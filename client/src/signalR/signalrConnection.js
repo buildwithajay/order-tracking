d7ec101191d5232bc1b1781a5b488c1f3d286afe
heyitsajay <poudelajay547@gmail.com>
2026-01-30 21:41:54 +0530

    fixing the hub url

--- client/src/signalR/signalrConnection.js
+++ client/src/signalR/signalrConnection.js
@@ -1,15 +1,18 @@
 import * as signalR from "@microsoft/signalr";
 
+
+const API_BASE = import.meta.env.VITE_API_BASE;
 let connection = null;
 
 export const getConnection = () => connection;
+
 
 export const startConnection = async (token) => {
   if (connection) return connection;
 
   connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://jolly-water-023db5f00.6.azurestaticapps.net/orderHub", {
-      accessTokenFactory: () => token,
+    .withUrl(`${API_BASE}/orderHub`, {
+      accessTokenFactory: () => localStorage.getItem("token") || ""
     })
     .withAutomaticReconnect()
     .build();
